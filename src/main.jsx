fd1c81565228f2de5ed4297896812f220bf1f6c2
jepirr <vieribogi19@gmail.com>
2026-01-15 05:12:53 +0700

    feat: update UI components, add auth & theme context, fix dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import './App.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { ThemeContextProvider } from "./context/themeContext.jsx";
+import { AuthContextProvider } from "./context/authContext.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeContextProvider>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </ThemeContextProvider>
+  </React.StrictMode>
+);
